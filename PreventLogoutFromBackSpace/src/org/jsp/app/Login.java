a60d01b690223c9c1792545226b2021b6adb46f7
guruvinay29 <61902262+guruvinay29@users.noreply.github.com>
2020-06-08 17:46:16 +0530

    Jsp Login

---
+++ PreventLogoutFromBackSpace/src/org/jsp/app/Login.java
@@ -0,0 +1,31 @@
+package org.jsp.app;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		
+		if (uname.equals("guruvinay")&&pass.equals("329")) {
+			
+			HttpSession session =  request.getSession();
+			session.setAttribute("username", uname);
+			response.sendRedirect("welcome.jsp");
+			
+		}
+		
+	}
+
+	
+}
